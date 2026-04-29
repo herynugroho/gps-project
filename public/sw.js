3a32c49ea46211446c542c8fb3ac07e9d10eae7a
herynugroho <herynugie@gmail.com>
2026-04-29 11:57:28 +0800

    v2 PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'primatrack-v1';
+const CACHE_NAME = 'primatrack-v2';
 
 // Install Service Worker
 self.addEventListener('install', event => {
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <!-- PWA Setup -->
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#0f172a">
-    <link rel="apple-touch-icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/GPS_Icon.svg/512px-GPS_Icon.svg.png">
+    <link rel="apple-touch-icon" href="/icon.png">
     
     <script>
         if ('serviceWorker' in navigator) {
@@ -13,7 +13,7 @@
             });
         }
     </script>
-    
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Login - PrimaTrack</title>
